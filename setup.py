cfc702e587b1f2bb247dad920ef857d925fffdb1
atzm <atzm@atzm.org>
2018-05-22 00:11:16 +0900

    setup: add long description

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
     name='cefp',
     version='0.0.1',
     description='ArcSight CEF Parser',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Atzm Watanabe',
     author_email='atzm@atzm.org',
     entry_points={'console_scripts': ['cefp = cefp:main']},
